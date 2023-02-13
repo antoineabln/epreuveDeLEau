d793a28f1c6de88f449268669087d2b8e558384a
antoineabln <johndoe@example.com>
2023-02-13 21:25:29 +0100

    programme qui affiche la différence minimum absolue entre deux éléments d’un tableau constitué uniquement de nombres

---
+++ eau10.js
@@ -0,0 +1,29 @@
+
+let tab =["Ceci", "est", "le", "monde", "qui", "contient", "Charlie", "un", "homme", "sympa", "Charlie"];
+
+function index(){
+  
+  let dernierElement = tab[tab.length-1];
+  let c = [];
+  
+  for(let i = 0; i<tab.length-1; i++){
+    
+    if(tab[i] === dernierElement){
+      
+      c.push(i);
+      
+    }
+  }
+  
+  if(c.length === 0){
+    
+    console.log(-1);
+    
+  }else{
+    
+    console.log(c[0]);
+  }
+
+}
+
+index(tab);
